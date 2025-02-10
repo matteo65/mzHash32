93b4e866787c0e8def2b715b6ebe111d93a3f4f3
Matteo Zapparoli <zapparoli.matteo@gmail.com>
2025-02-10 11:58:25 +0100

    Update mzhash32.h

--- src/C/mzhash32.h
+++ src/C/mzhash32.h
@@ -1,6 +1,7 @@
 #ifndef _MZHASH32_H
 #define _MZHASH32_H
 
+#include <stddef.h> // size_t
 #include <stdint.h> // uint32_t
 
 #ifdef __cplusplus
@@ -9,7 +10,7 @@ extern "C" {
 
 #define MZ_HASH_32_DEFAULT_SEED		0;
 
-uint32_t mzhash32(const int8_t *data, int length, uint32_t seed);
+uint32_t mzhash32(const char* data, size_t length, uint32_t seed);
 
 #ifdef __cplusplus
 }
